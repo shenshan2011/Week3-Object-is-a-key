f5e9da32192d4a42e465d2244d25ce2b3e9f4175
shenshan2011 <dana712.sesatism@gmail.com>
2025-03-21 09:12:51 +0700

    soal3-part3-week3

--- quiz/part3/soal3.js
+++ quiz/part3/soal3.js
@@ -12,32 +12,57 @@ Function countProfit akan mengembalikan/me-return sebuah array of object dimana 
 */
 
 function countProfit(shoppers) {
-    let listBarang = [ ['Sepatu Stacattu', 1500000, 10],
+    var listBarang = [ ['Sepatu Stacattu', 1500000, 10],
                        ['Baju Zoro', 500000, 2],
                        ['Sweater Uniklooh', 175000, 1]
                      ];
   
+    // you can only write your code here!
     if (shoppers.length === 0) {
-        return [];
+      return []
     }
-
-    let result = listBarang.map(([product, price, stock]) => ({
-        product,
-        shoppers: [],
-        leftOver: stock,
-        totalProfit: 0
-    }));
-
-    for (let {name, product, amount} of shoppers) {
-        let item = result.find(item => item.product === product)
-        let hargabarang = listBarang.find(([p]) => p === product)
-        if (item && hargabarang && item.leftOver >= amount) {
-            item.shoppers.push(name);
-            item.leftOver -= amount
-            item.totalProfit += amount * hargabarang[1];
+    let sepatu = {
+      product: listBarang[0][0],
+      shoppers: [],
+      leftOver: listBarang[0][2],
+      totalProfit: 0
+    }
+    let baju = {
+      product: listBarang[1][0],
+      shoppers: [],
+      leftOver: listBarang[1][2],
+      totalProfit: 0
+    }
+    let sweater = {
+      product: listBarang[2][0],
+      shoppers: [],
+      leftOver: listBarang[2][2],
+      totalProfit: 0
+    }
+  
+    for (let i = 0; i < shoppers.length; i++) {
+      if (listBarang[0][0] === shoppers[i]['product']) {
+        if (shoppers[i].amount <= sepatu.leftOver) {
+          sepatu.shoppers.push(shoppers[i].name)
+          sepatu.leftOver -= shoppers[i].amount
+          sepatu.totalProfit += shoppers[i].amount * listBarang[0][1]
         }
+      } else if (listBarang[1][0] === shoppers[i]['product']) {
+        if (shoppers[i].amount <= baju.leftOver) {
+          baju.shoppers.push(shoppers[i].name)
+          baju.leftOver -= shoppers[i].amount
+          baju.totalProfit += shoppers[i].amount * listBarang[1][1]
+        }
+      } else {
+        if (shoppers[i].amount <= sweater.leftOver) {
+          sweater.shoppers.push(shoppers[i].name)
+          sweater.leftOver -= shoppers[i].amount
+          sweater.totalProfit += shoppers[i].amount * listBarang[2][1]
+        }
+      }
     }
-    return result;
+    
+    return [sepatu, baju, sweater]
   }
   
   // TEST CASES
